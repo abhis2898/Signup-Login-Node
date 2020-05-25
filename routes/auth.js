759db398f9278ecb17d5250d8b0baac7d499c90f
Abhishek Sharma <33370994+abhis2898@users.noreply.github.com>
2020-05-26 03:43:13 +0530

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const authController = require("../controllers/auth");
+
+const router = express.Router();
+
+router.post('/register', authController.register );
+
+router.post('/login', authController.login );
+
+router.post('/profile', authController.profile );
+
+module.exports = router;
